a452bd85716d7d03fa38ef8f1722b508e87962ac
maslow <wangfugen@126.com>
2022-03-06 00:44:37 +0800

    fix: fix error;

--- src/cloud.ts
+++ src/cloud.ts
@@ -1,6 +1,6 @@
 
 import { Cloud } from 'laf-client-sdk'
-import { getToken } from './api/tokenn'
+import { getToken } from './api/token'
 
 export const cloud = new Cloud({
   baseUrl: 'https://7dd2f8e8-6102-492c-a522-b5a7db2ab00a.lafyun.com',
