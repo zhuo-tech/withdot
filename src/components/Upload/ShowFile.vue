ec44960d03fd1e8eba362787cd3822b1ed29b363
DNN <1524757775@qq.com>
2022-03-31 18:56:14 +0800

    fix: 增加表单验证

--- src/components/Upload/ShowFile.vue
+++ src/components/Upload/ShowFile.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import { Picture as IconPicture } from '@element-plus/icons-vue'
+import { ImageProps } from 'element-plus'
+
+const propsValue = defineProps<ImageProps>()
 
 </script>
 
 <template>
-
+<el-image>
+    <template #error>
+        <div class="image-slot">
+            <el-icon><IconPicture/></el-icon>
+        </div>
+    </template>
+</el-image>
 </template>
 
 <style scoped>
