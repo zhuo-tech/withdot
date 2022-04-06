254d2d71505d6150f4d02c4b1a42c64c1f59fdee
丁延康 <1120252308@qq.com>
2022-04-06 12:58:03 +0800

    fix:getUserInfo

--- src/pages/accounts/profile.vue
+++ src/pages/accounts/profile.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import { getUserInfo }  from '@/api/token'
+import { ElMessage } from 'element-plus'
+import { onMounted } from 'vue'
+
+const getInfo = () => {
+    getUserInfo().then(response=>{
+        console.log(response)
+    }).catch(err=>{
+        ElMessage.error(err)
+    })
+}
+onMounted(()=>{
+    getInfo()
+})
+</script>
 
 <template>
-<div>账户信息</div>
+    <div>账户信息</div>
 </template>
 
-<style scoped lang="less"></style>
+<style lang="less" scoped></style>
