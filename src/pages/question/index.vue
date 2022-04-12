e1b92237f45d3725edcb42c66e80e39ede76f4a6
丁延康 <1120252308@qq.com>
2022-04-12 15:23:54 +0800

    fix:题库管理

---
+++ src/pages/question/index.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import QuestionService from '@/pages/question/QuestionService'
+import { reactive } from 'vue'
+import QueryForm from './components/QueryForm.vue'
+import Form from './components/Form.vue'
+
+const service = reactive(new QuestionService())
+</script>
+
+<template>
+    <el-card class="box-card">
+        <template #header>
+            <div>题库管理</div>
+        </template>
+        <!--功能栏-->
+        <QueryForm :service="service" />
+    </el-card>
+    <Form :service="service" />
+</template>
+
+<style lang="less" scoped>
+</style>
