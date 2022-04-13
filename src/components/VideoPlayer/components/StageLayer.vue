7b7656cd475c2c6fa321e3c018d579747f82c5f5
DNN <1524757775@qq.com>
2022-04-13 19:44:44 +0800

    fix: ...

--- src/components/VideoPlayer/components/StageLayer.vue
+++ src/components/VideoPlayer/components/StageLayer.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
+import { StageLayerContext } from '@/components/VideoPlayer/context/StageLayerContext'
 import { CoreDot } from '@/model/entity/CoreDot'
+import { onMounted, reactive } from 'vue'
 import Draggable from './Draggable.vue'
 
 /**
@@ -11,11 +13,28 @@ const props = defineProps<{
     list: Array<CoreDot>
 }>()
 
+const emits = defineEmits<{
+    /**
+     * 通知有鼠标离开了拖动容器
+     */
+    (event: 'leave', time: number): void
+
+}>()
+
+const context = reactive(new StageLayerContext())
+
+onMounted(() => context.updateReferencePoint())
 </script>
 
 <template>
-<div class="stage-box">
-    <Draggable v-for="(item, index) in list" :key="index" :index="index" :item="item" />
+<div class="stage-box" @mousemove="event => event" :ref="el => context.stageLayerRef = el">
+    <Draggable v-for="(item, index) in list"
+               :key="index"
+               :index="index"
+               :item="item"
+               @mousedown="event => event"
+               @mouseleave="emits('leave', Date.now())"
+               @mouseup="event => event " />
 </div>
 </template>
 
