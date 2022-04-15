2ccfb1c6796ba8b217ab6b39c592a9fba980dd0a
丁延康 <1120252308@qq.com>
2022-04-15 13:51:50 +0800

    fix:

--- src/components/workConfigForm/TextForm.vue
+++ src/components/workConfigForm/TextForm.vue
@@ -1,39 +1,45 @@
 <script lang="ts" setup>
-import { reactive } from 'vue'
 import Editor from '@/components/editor/Editor.vue'
 
-const config = reactive({
-    text: '',
-    switch: false,
-    time:3,
-    pause:true
+
+const props = defineProps({
+    value: {
+        type: Object,
+        required: true,
+        default:()=>({
+            switch: false,
+            time: 3,
+            pause: true,
+        })
+    },
 })
-const changeValue = (value: any) => {
-    config.text = value
-}
 </script>
 
 <template>
-    <el-form ref="form" :model="config" label-width="80px">
+    <el-form ref="form" :model="value" label-width="80px" class="box">
         <el-form-item label="文本内容">
-            <Editor v-model:value="config.text" @changeValue="changeValue" />
+            <Editor v-model:value="value.text" @changeValue="changeValue" />
         </el-form-item>
         <el-row  style="margin-top: 20px">
             <el-col :span="12">
                 <el-form-item label="设置时长">
-                    <el-switch v-model="config.switch"></el-switch>
+                    <el-switch v-model="value.switch"></el-switch>
                 </el-form-item>
             </el-col>
-            <el-col :span="12" v-if="config.switch">
+            <el-col :span="12" v-if="value.switch">
                 <el-form-item label="时长">
-                    <el-input-number v-model="config.time" :min="1"></el-input-number>&nbsp;&nbsp;&nbsp;秒
+                    <el-input-number v-model="value.time" :min="1"></el-input-number>&nbsp;&nbsp;&nbsp;秒
                 </el-form-item>
             </el-col>
         </el-row>
         <el-form-item label="是否暂停">
-            <el-switch v-model="config.pause" ></el-switch>
+            <el-switch v-model="value.pause" ></el-switch>
         </el-form-item>
     </el-form>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+:deep(.el-form-item__label) {
+    z-index:0;
+}
+</style>
