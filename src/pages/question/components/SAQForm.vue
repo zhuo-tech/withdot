3019cb96bcaabfc0118d9e2207894ef89bc38390
丁延康 <1120252308@qq.com>
2022-04-18 14:22:26 +0800

    fix:题库管理

--- src/pages/question/components/SAQForm.vue
+++ src/pages/question/components/SAQForm.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 import QuestionService, { questionTypeList } from '@/pages/question/QuestionService'
-import Editor from '@/components/editor/Editor.vue'
 
 const props = defineProps<{
     service: QuestionService
@@ -12,25 +11,19 @@ const props = defineProps<{
                destroy-on-close
                :title="service.formStatus? '新建题目':'编辑题目'"
                top="2vh"
-               width="70%"
+               width="40%"
                @close="service.formData.initForm()">
-        简答
         <el-form :ref="el => service.formRef = el" :model="service.formData.form" :rules="service.rules" label-width="80px">
-            <el-form-item label="题目标题" prop="label">
-                <el-input v-model="service.formData.form.label" placeholder="请输入题目标题"></el-input>
-            </el-form-item>
-            <el-form-item label="题目类型" prop="type">
-                <el-select v-model="service.formData.form.type" placeholder="请选择题目类型">
-                    <el-option v-for="item in questionTypeList" :key="item" :label="item.label" :value="item.value" />
-                </el-select>
-            </el-form-item>
-            <el-form-item label="题目内容">
-                <Editor v-model:value="service.formData.form.content" @changeValue="service.changeValue" />
+            <el-form-item label="题目" prop="label">
+                <el-input v-model="service.formData.form.label"
+                          :autosize="{ minRows: 2, maxRows: 4 }"
+                          placeholder="请输入题目内容"
+                          type="textarea" />
             </el-form-item>
         </el-form>
         <template #footer>
         <span class="dialog-footer">
-            <el-button @click="service.formData.close()">取 消</el-button>
+            <el-button @click="service.formData.allClose()">取 消</el-button>
             <el-button :loading="service.formData.formIsLoading" type="primary" @click="service.formSubmit">
                 {{ service.formData.formIsLoading ? '提交中 ...' : '确 定' }}
             </el-button>
