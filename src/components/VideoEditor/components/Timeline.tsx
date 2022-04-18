8928c72bb95679e0a07cf0d105255a17348f980c
DNN <1524757775@qq.com>
2022-04-19 01:43:33 +0800

    style: 补充注释

--- src/components/VideoEditor/components/Timeline.tsx
+++ src/components/VideoEditor/components/Timeline.tsx
@@ -62,7 +62,13 @@ export default defineComponent({
         } as any
     },
     methods: {
-        getPeriod(start: number, end: number, timeUnit: TimeUnit) {
+        /**
+         * @param {number} start 单位 秒
+         * @param {number} end 单位 秒
+         * @param {TimeUnit} timeUnit 时间单位
+         * @return {Array<Period>} 给定 timeUnit 作为粒度, 能够划分的所有时间段. 可能存在 Period 不足一个完整的单位
+         */
+        getPeriod(start: number, end: number, timeUnit: TimeUnit): Array<Period> {
             const toSecond = timeUnit.toSecond(1)
             const res = []
             while (true) {
@@ -78,19 +84,28 @@ export default defineComponent({
             return res
         },
 
+        /**
+         * 渲染一个小时 (下的所有分钟刻度)
+         * @param {Period} period 以小时为单位划分的时间段
+         */
         renderHour(period: Period) {
             const periodArr = this.getPeriod(period.start, period.end, TimeUnit.MINUTE)
             return (
-                <div class="timeline-item hour" style={{flexGrow: periodArr.length}}>
+                <div class="timeline-item hour" style={ {flexGrow: periodArr.length} }>
                     { periodArr.map((period, index) => this.renderMinute(period, index == 0)) }
                 </div>
             )
         },
 
+        /**
+         * 渲染一分钟(下的所有秒刻度)
+         * @param {Period} period 以分钟为单位 划分的时间段
+         * @param {boolean} showStart 是否显示 start tips; end tips 自动 在 小于 一半时隐藏, 大于一半时显示;
+         */
         renderMinute(period: Period, showStart: boolean) {
             const periodArr = this.getPeriod(period.start, period.end, TimeUnit.SECOND)
             return (
-                <div class="timeline-item minute" style={{flexGrow: periodArr.length}}>
+                <div class="timeline-item minute" style={ {flexGrow: periodArr.length} }>
                     {
                         showStart && (
                             <div class="start-tips">
@@ -109,13 +124,16 @@ export default defineComponent({
                         periodArr.map(second => {
                             const isCurrent = Math.round(this.current) === second.start
                             const className = isCurrent ? 'timeline-item second current-item' : 'timeline-item second'
-                            return (<div class={className} data-time-value={ second.start }></div>)
+                            return (<div class={ className } data-time-value={ second.start }></div>)
                         })
                     }
                 </div>
             )
         },
 
+        /**
+         * 在鼠标悬停时, 移动悬浮刻度指针
+         */
         onMouseover(event: MouseEvent) {
             const ele: HTMLDivElement = event.target as any
             if (!ele.className.includes('second')) {
@@ -125,6 +143,9 @@ export default defineComponent({
             this.hoverLeft = ele.getBoundingClientRect().right - this.containerRect.left
         },
 
+        /**
+         * 在鼠标单击时, 声明被选择时间
+         */
         onMouseclick(event: MouseEvent) {
             const ele: HTMLDivElement = event.target as any
             if (!ele.className.includes('second')) {
